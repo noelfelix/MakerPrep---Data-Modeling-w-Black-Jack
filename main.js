2c18e5dfa370b6d3a5c48c67670ef5fbb4a80c90
Noel Felix <noel.r.felix@gmail.com>
2016-05-21 21:10:53 -0700

    Simple Example Complete

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-function cardMaker (value, suit) {
+function extend (obj1, obj2) {
+  for (var key in obj2) {
+    obj1[key] = obj2[key];
+  }
+  return obj1;
+}
+
+function cardMaker (rank, suit) {
   return {
-    value: value,
+    rank: rank,
     suit: suit
   }
 }
 
+function blackjackCardMaker (rank, suit, value) {
+  return extend(cardMaker(rank,suit), {value: value});
+}
+
 function deckMaker() {
   var deck = [];
-  var values = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+  var ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  var values = [0,2,3,4,5,6,7,8,9,10,10,10,10];
   var suits = ['S','D','H','C'];
 
   for (var i = 0; i < suits.length; i++) {
-    for (var j = 0; j < values.length; j++) {
-      deck.push(cardMaker(values[j], suits[i]));
+    for (var j = 0; j < ranks.length; j++) {
+      deck.push(blackjackCardMaker(ranks[j], suits[i], values[j]));
     }
   }
   return deck;
@@ -26,28 +38,14 @@ function playerMaker(name) {
 }
 
 function displayCard (card) {
-  var display = '';
-
-  var faceCards = {
-    1: 'A',
-    11: 'J',
-    12: 'Q',
-    13: 'K'
-  };
-
-  if (faceCards[card.value]) {
-    display += faceCards[card.value];
-  } else {
-    display += card.value;
-  }
-  return display += card.suit;;
+  return card.rank + card.suit;;
 }
 
 function displayDeck (deck) {
   var display = '';
   for (var i = 0; i < deck.length; i++) {
     display += displayCard(deck[i]) + ' ';
-    if (i % 5 === 4) {
+    if (i % 13 === 12) {
       display += '\n';
     }
   }
@@ -56,23 +54,32 @@ function displayDeck (deck) {
 
 function shuffle (deck) {
   var shuffled = [];
-  var count = deck.length;
-  for (var i = 0; i < count; i++) {
+  var cards = deck.length;
+  for (var i = 0; i < cards; i++) {
     shuffled.push(deck.splice(Math.floor(Math.random() * deck.length),1)[0]);
   }
   return shuffled;
 }
-//WRITE WITH MULTIPLE LINES
+
 function shuffle2 (deck) {
   for (var i = 0; i < deck.length; i++) {
-    deck.push(deck.splice(Math.floor(Math.random() * deck.length - i),1)[0]);
+    deck.push(deck.splice(Math.floor(Math.random() * deck.length - i), 1)[0]);
   }
   return deck;
 }
 
-var deck = deckMaker(); shuffle2(deck); console.log(deck)
-
-
+//Best
+function shuffle3 (deck) {
+  var randInd1, randInd2, temp;
+  for (var i = 0; i < 30; i++) {
+    randInd1 = Math.floor(Math.random() * deck.length);
+    randInd2 = Math.floor(Math.random() * deck.length);
+    temp = deck[randInd1];
+    deck[randInd1] = deck[randInd2];
+    deck[randInd2] = temp;
+  }
+  return deck;
+}
 
 function deal (deck, player) {
   if (deck.length === 0) {
@@ -84,18 +91,19 @@ function deal (deck, player) {
 
 function handValue(hand) {
   var totalValue = 0;
-  hand.sort(function(a,b){return b.value > a.value});
+  var isAce = false;
   for (var i = 0; i < hand.length; i++) {
-    if (hand[i].value === 1) {
-      if (totalValue + 10 > 21) {
-        totalValue += 1;
-      } else {
-        totalValue += 10;
-      }
-    } else if (hand[i].value >=10) {
-      totalValue += 10;
+    if (hand[i].rank === 'A') {
+      isAce = true;
     } else {
       totalValue += hand[i].value;
+    }
+  }
+  if (isAce) {
+    if (totalValue < 11) {
+      totalValue += 11;
+    } else {
+      totalValue += 1;
     }
   }
   return totalValue;
@@ -107,17 +115,57 @@ function displayPlayerHand (player) {
   for (var i = 0; i < player.hand.length; i++) {
     handToJoin.push(displayCard(player.hand[i]));
   }
-  return display += handToJoin.join(', ') + " : " + handValue(player.hand);
+  var value = handValue(player.hand);
+  var status = '';
+  if (value > 21) {
+    status = ' BUST';
+  }
+  if (value === 21) {
+    status === " BLACKJACK";
+  }
+  return display += handToJoin.join(', ') + " : " + value + status;
 }
 
 function play() {
-  var deck = shuffle(deckMaker());
+  var deck = shuffle3(deckMaker());
+  var player1 = playerMaker('Player One');
+  var player2 = playerMaker('Player Two');
 
+  while (handValue(player1.hand) < 17) {
+    deal(deck, player1);
+  }
+  while (handValue(player2.hand) < 17) {
+    deal(deck, player2);
+  }
+  console.log(displayPlayerHand(player1));
+  console.log(displayPlayerHand(player2));
+
+  var p1 = handValue(player1.hand);
+  var p2 = handValue(player2.hand);
+
+  if(p1 > 21 && p2 > 21) {
+    console.log("\nGAME OVER - BOTH BUSTED!");
+  } else if (p1 > 21 || p2 > 21) {
+    console.log("\nGAME OVER - " + (p1 > 21 ? player2.name : player1.name) + " WINS!");
+  } else if (p1 === p2) {
+    console.log("\nGAME OVER - TIED!");
+  } else {
+    console.log("\nGAME OVER - " + (p1 > p2 ? player1.name : player2.name) + " WINS!!");
+  }
 }
 
-var deck = shuffle(deckMaker());
-var player1 = playerMaker("Noel");
-deal(deck, player1);
-deal(deck, player1);
-deal(deck, player1);
-console.log(displayPlayerHand(player1));
+play();
+
+//Testing shuffle speeds
+// function shuffleTest (shuf, count) {
+//   var deck = deckMaker();
+//   var start = new Date();
+//   for (var i = 0; i < count; i++) {
+//     deck = shuf(deck);
+//   }
+//   return "\nCount: " + count + '\nTime: ' + (new Date() - start);
+// }
+//
+// console.log(shuffleTest(shuffle3, 100000));
+// console.log(shuffleTest(shuffle2, 100000));
+// console.log(shuffleTest(shuffle, 100000));
